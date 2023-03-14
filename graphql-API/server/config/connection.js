e1042fd248e0ea12b3b80c9720c7fda0eb8c3de6
roddazad <roddazad@gmail.com>
2023-03-14 10:37:39 -0700

    trying

--- graphql-API/server/config/connection.js
+++ graphql-API/server/config/connection.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+// );
